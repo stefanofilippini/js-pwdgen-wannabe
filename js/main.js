fa8f9eff50ef68eb9f54fb23a09d1993d42f6fde
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-20 15:06:30 +0200

    done exercise

--- js/main.js
+++ js/main.js
@@ -7,4 +7,5 @@
 const nome = prompt ('inserisci qui il tuo nome');
 const cognome = prompt ('inserisci qui il tuo cognome');
 const colore = prompt ('inserisci qui il tuo colore preferito');
-console.log(nome + cognome + colore + '21');
+document.getElementById("password").innerHTML= nome + cognome + colore + '21';
+//document.getElementById("password").innerHTML= `La tua password è : ${nome}${cognome}${colore}21`;

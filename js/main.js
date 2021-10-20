a769470085214bddd09554086df7910531302c34
stefanofilippini <filippini.stefano96@gmail.com>
2021-10-20 14:50:10 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Password generator:
+// - chiedi all’utente il suo nome,
+// - poi chiedi il suo cognome,
+// - poi chiedi il suo colore preferito
+// - infine scrivi sulla pagina i dati ricevuti nel formato: nomecognomecolorepreferito21
+
+const nome = prompt ('inserisci qui il tuo nome');
+const cognome = prompt ('inserisci qui il tuo cognome');
+const colore = prompt ('inserisci qui il tuo colore preferito');
+console.log(nome + cognome + colore + '21');
